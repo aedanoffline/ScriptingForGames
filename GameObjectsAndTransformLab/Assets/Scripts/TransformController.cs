8f74d86ddadd3bf80de1d5a0a5a0e2aa001b98df
Aidan Reed <aidan.reed@uvu.edu>
2024-09-11 18:23:08 -0600

    Tweaked TransformController Scale Feature

--- GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class TransformController : MonoBehaviour
 {
-    public GameObject TargetTransform;
+    public GameObject targetTransform;
     private void Update()
     {
         // Move the target GameObject
         var x = Mathf.PingPong(Time.time, 3);
         var p = new Vector3(0, x, 0);
-        TargetTransform.transform.position = p;
+        Transform box = targetTransform.transform;
+        
+        box.position = p;
         
         // Rotate the target GameObject
-        TargetTransform.transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        box.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
         
         //Ping pong box scale
-        TargetTransform.transform.localScale = new Vector3(1, x, 1);
+        box.localScale = new Vector3(box.localScale.x, x, box.localScale.z);
     }
 }
