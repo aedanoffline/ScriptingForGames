cfa65a8c498fa40ffe9365f1e575d85912a33b94
Aidan Reed <aidan.reed@uvu.edu>
2024-09-23 18:07:25 -0600

    Made character and platform

---
+++ AdventureGame/Assets/Scripts/SimpleCharacterController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCharacterController : MonoBehaviour
+{
+   public float moveSpeed = 5f;
+   private CharacterController controller;
+   private Transform thisTransform;
+   private Vector3 movementVector = Vector3.zero;
+   
+   private void Start()
+}
