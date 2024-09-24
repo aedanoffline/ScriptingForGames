56afd05bb04e2ade340b5ac147a70c5224c4dd0b
Aidan Reed <aidan.reed@uvu.edu>
2024-09-23 18:27:02 -0600

    Added character movement and comments

--- AdventureGame/Assets/Scripts/SimpleCharacterController.cs
+++ AdventureGame/Assets/Scripts/SimpleCharacterController.cs
@@ -8,6 +8,36 @@ public class SimpleCharacterController : MonoBehaviour
    private CharacterController controller;
    private Transform thisTransform;
    private Vector3 movementVector = Vector3.zero;
-   
+
    private void Start()
+   {
+       // Establishes component references
+       controller = GetComponent<CharacterController>();
+       thisTransform = transform;
+   }
+
+   private void Update()
+   {
+       // Updates to ensure movement and z axis locking are available at all times
+       MoveCharacter();
+       KeepCharacterOnXAxis();
+   }
+
+   private void MoveCharacter()
+   {
+       // Assigns the X of the vector to the horizontal input axis
+       movementVector.x = Input.GetAxis("Horizontal");
+       // Multiplies this input by a factor of moveSpeed (defined above)
+       movementVector *= (moveSpeed * Time.deltaTime);
+       // Applies this newly updated vector to the character controller's move function
+       controller.Move(movementVector);
+   }
+
+   private void KeepCharacterOnXAxis()
+   {
+       // Assigns "0" to the transform properties of the script parent
+       var currentPosition = thisTransform.position;
+       currentPosition.z = 0f;
+       thisTransform.position = currentPosition;
+   }
 }
