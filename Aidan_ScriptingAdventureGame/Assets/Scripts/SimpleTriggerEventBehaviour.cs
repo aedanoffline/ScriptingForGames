7b691e25f160a0037e6dcb828736c3324e36c650
Aidan Reed <aidan.reed@uvu.edu>
2024-10-07 18:43:24 -0600

    Added audio event to spike trigger

--- Aidan_ScriptingAdventureGame/Assets/Scripts/SimpleTriggerEventBehaviour.cs
+++ Aidan_ScriptingAdventureGame/Assets/Scripts/SimpleTriggerEventBehaviour.cs
@@ -4,9 +4,15 @@ using UnityEngine.Events;
 public class SimpleTriggerEventBehaviour : MonoBehaviour
 {
     public UnityEvent triggerEvent;
+    public AudioSource audioSource; // Reference to the AudioSource
+    public float soundOffset = 0.03f;
     
     private void OnTriggerEnter(Collider other)
     {
+        //Call up audio source and change the time offset
+        audioSource.GetComponent<AudioSource>();
+        audioSource.time = soundOffset;
+        
         //Trigger event with debug message
         triggerEvent.Invoke();
         Debug.Log("Player interacted with the object!");
